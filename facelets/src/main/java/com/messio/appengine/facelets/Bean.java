eb0d7b6fe04757805265479a2a261a64558beca3
jpc <jpcuvelliez@gmail.com>
2016-12-29 18:03:58 +0100

    .

--- facelets/src/main/java/com/messio/appengine/facelets/Bean.java
+++ facelets/src/main/java/com/messio/appengine/facelets/Bean.java
@@ -1,15 +1,41 @@
 package com.messio.appengine.facelets;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jpc on 12/28/16.
  */
 @ManagedBean
-@RequestScoped
-public class Bean {
+@ViewScoped
+public class Bean implements Serializable {
+    private List<String> texts = new ArrayList<>();
+    private String text;
+
     public String getTest(){
         return "this string is coming from a jsf bean";
+    }
+
+    public String addText(){
+        if (text != null && text.trim().length() > 0){
+            texts.add(text.trim());
+            text = null;
+        }
+        return null;
+    }
+
+    public List<String> getTexts() {
+        return texts;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
